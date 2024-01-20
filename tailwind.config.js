ddd5c5cb78e5d23fb976146f4ed8afc104852e0a
Ibrahim Gunes <gunesibrahim.x@gmail.com>
2024-01-20 13:36:19 +0300

    My first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.tsx',
+        "./node_modules/flowbite/**/*.js",
+        'node_modules/flowbite-react/lib/esm/**/*.js'
     ],
 
     theme: {
@@ -18,5 +20,5 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [forms,require('flowbite/plugin')],
 };
